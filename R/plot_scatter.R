6a8939a3a771f4aa5636998e717f8c06f1e8223f
Hao Wu <haow@rho-oak.stc.ricplc.com>
2021-07-07 15:46:22 +0100

    improve method to calculate fitted data and equation

--- R/plot_scatter.R
+++ R/plot_scatter.R
@@ -74,10 +74,10 @@ plot_scatter <- function(data, x, y, group = NULL, text = NULL, hoverinfo = "x+y
   }
 
   # prepare data drop NAs
-  data <- prep_data(data, x, y, group, text)
+  dat <- prep_data(data, x, y, group, text)
 
   # plotting
-  scatter <- data %>%
+  scatter <- dat %>%
     plotly::plot_ly(x = ~x, ...) %>%
     plotly::add_markers(y = ~y, color = ~group, hoverinfo = hoverinfo, text = ~text,
                         colors = colors) %>%
@@ -85,37 +85,34 @@ plot_scatter <- function(data, x, y, group = NULL, text = NULL, hoverinfo = "x+y
            yaxis = list(title = y))
 
   # calculate linear line if there are at least 2 data points
-  if (linear && NROW(data) >= 2 && !is.numeric(data[["group"]])) {
+  if (linear && NROW(dat) >= 2 && !is.numeric(dat[["group"]])) {
 
     # separate lm model for each group
-    fit <- data %>%
-      group_by(group) %>%
-      group_map(~lm(y ~ x, data = .))
+    fit <- dat %>%
+      tidyr::nest(data = c(-group)) %>%
+      mutate(model = purrr::map(data, ~lm(y ~ x, .x)))
 
-    names(fit) <- unique(data$group)
-
-    # prepare data frame for the fitted lines
-    fitted_data <- data.frame(
-      y = purrr::map(fit, fitted) %>% unlist(),
-      x = data$x,
-      group = data$group
-    )
+    # create a fitted y data frame
+    fitted_data <- fit %>%
+      mutate(y_fitted = purrr::map(model, fitted)) %>%
+      tidyr::unnest(cols = c(data, y_fitted))
 
     # prepare data frame for lm equations
-    fitted_eqs <- fit %>%
-      purrr::map_df(~as.data.frame(rbind(round(coef(.x), 2))), .id = "group") %>%
-      rename(intercept = `(Intercept)`) %>%
-      mutate(
-        rsq = purrr::map_dbl(fit, ~round(summary(.x)$r.squared, 2)),
-        form = paste0("y = ", x, "x ", ifelse(intercept < 0, "", "+"), intercept, ", r2 = ", rsq),
-        x = max(data$x, na.rm = TRUE) * eq_x,
-        y = seq(max(data$y, na.rm = TRUE) * eq_y,
-                by = sd(data$y, na.rm = TRUE) * 0.5,
-                length.out = n()))
+    xmax <- max(dat$x)
+    ymax <- max(dat$y)
 
+    fitted_eqs <- fit %>%
+      mutate(slope = purrr::map_dbl(model, ~round(coefficients(.x)["x"], 2)),
+             intercept = purrr::map_dbl(model, ~round(coefficients(.x)["(Intercept)"]), 2),
+             rsq = purrr::map_dbl(model, ~round(summary(.x)$r.squared, 2)),
+             form = paste0("y = ", slope, "x ", ifelse(intercept < 0, "", "+"), intercept, ", r2 = ", rsq),
+             x = xmax * eq_x,
+             y = seq(ymax * eq_y,
+                     by = sd(dat$y) * 0.5,
+                     length.out = n()))
 
     scatter <- scatter %>%
-      plotly::add_lines(y = ~y, x = ~x, color = ~group, data = fitted_data) %>%
+      plotly::add_lines(y = ~y_fitted, x = ~x, color = ~group, data = fitted_data) %>%
       plotly::add_text(y = ~y, x = ~x, text = ~form, color = ~group, data = fitted_eqs,
                        showlegend = FALSE)
 
